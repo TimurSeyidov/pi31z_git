f5d798d7dfa29d841d1f1508136fe1bab4dcb044
Timur Seyidov <tim@jlabs.pro>
2024-11-20 11:17:32 +0500

    added Knight

---
+++ src/Knight.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once('IFigure.php');
+
+class Knight extends Figure {
+    protected string $icon = 'K';
+
+    public function canMove(int $from_row, int $from_col, int $to_row, int $to_col, Board $board): bool {
+        $diff_row = abs($from_row - $to_row);
+        $diff_col = abs($from_col - $to_col);
+        return $diff_row == 2 && $diff_col == 1 || $diff_row == 1 && $diff_col == 2;
+    }
+
+    public function canAttack(int $from_row, int $from_col, int $to_row, int $to_col, Board $board): bool {
+        return $this->canMove($from_row, $from_col, $to_row, $to_col, $board);
+    }
+}
