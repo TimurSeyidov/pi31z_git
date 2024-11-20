533ddceb463924a1db3afb1fa18f0adb7b88da21
Timur Seyidov <tim@jlabs.pro>
2024-11-20 10:26:25 +0500

    added method canAttack + improved canMove method

--- src/Pawn.php
+++ src/Pawn.php
@@ -17,6 +17,23 @@ class Pawn extends Figure {
         if ($as_two_step_row) {
             $available[] = $from_row + $direction * 2;
         }
-        return in_array($to_row, $available);
+        if (!in_array($to_row, $available)) {
+            return false;
+        }
+        $ix = $from_row;
+        while ($ix != $to_row) {
+            $item = $board->getItem($ix, $from_col);
+            if ($item && $item != $this) {
+                return false;
+            }
+            $ix += $direction;
+        }
+        return true;
+    }
+
+    public function canAttack(int $from_row, int $from_col, int $to_row, int $to_col, Board $board): bool {
+        $diff_row = abs($to_row - $from_row);
+        $diff_col = abs($to_col - $from_col);
+        return $diff_col === 1 && $diff_row === 1;
     }
 }
